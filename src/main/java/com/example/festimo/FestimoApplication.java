ca0e40af7ac6f94b01ea55f09275fa7231a506aa
Taeyoung <teayoung789@naver.com>
2024-12-15 20:57:11 +0900

    Initial commit with dependencies

---
+++ src/main/java/com/example/festimo/FestimoApplication.java
@@ -0,0 +1,13 @@
+package com.example.festimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestimoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestimoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/festimo/FestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestimoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
