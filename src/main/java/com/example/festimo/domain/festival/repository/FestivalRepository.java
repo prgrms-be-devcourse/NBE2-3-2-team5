a905f5ebacc5fab697f1c63d3bad54f8fd57831b
juyeon6069 <jynam6069@gmail.com>
2024-12-18 17:50:08 +0900

    ✨ feat : 검색 & 필터링 구현

--- src/main/java/com/example/festimo/domain/festival/repository/FestivalRepository.java
+++ src/main/java/com/example/festimo/domain/festival/repository/FestivalRepository.java
@@ -2,6 +2,14 @@ package com.example.festimo.domain.festival.repository;
 
 import com.example.festimo.domain.festival.domain.Festival;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
 public interface FestivalRepository extends JpaRepository<Festival, String> {
+    List<Festival> findByTitleContainingIgnoreCase(String keyword);
+
+    List<Festival> findByAddressContainingIgnoreCase(String region);
 }
