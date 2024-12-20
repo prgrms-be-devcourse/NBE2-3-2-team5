e113f8aa5db48f33007ad948a082527fb5fea456
gyuri127 <117248176+gyuri127@users.noreply.github.com>
2024-12-20 11:16:49 +0900

    :fire: Feat: admin에서 fire삭제
    Related to: #37

--- src/main/java/com/example/festimo/domain/admin/mapper/AdminMapper.java
+++ src/main/java/com/example/festimo/domain/admin/mapper/AdminMapper.java
@@ -1,6 +1,6 @@
 package com.example.festimo.domain.admin.mapper;
 
-import com.example.festimo.domain.user.entity.Users;
+import com.example.festimo.domain.user.domain.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -14,12 +14,12 @@ public interface AdminMapper {
     AdminMapper INSTANCE = Mappers.getMapper(AdminMapper.class);
 
     // Entity → DTO 매핑
-    @Mapping(source = "userId", target = "userId")
+    @Mapping(source = "id", target = "userId") // User의 `id`를 AdminDTO의 `userId`로 매핑
     @Mapping(source = "userName", target = "userName")
     @Mapping(source = "nickname", target = "nickname")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "role", target = "role")
     @Mapping(source = "gender", target = "gender")
     @Mapping(source = "ratingAvg", target = "ratingAvg")
-    AdminDTO toDto(Users user);
+    AdminDTO toDto(User user);
 }
