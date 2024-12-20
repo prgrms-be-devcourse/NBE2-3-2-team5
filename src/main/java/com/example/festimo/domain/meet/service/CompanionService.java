810b0ac22d6fb9a786beff1f4df55d082eaf21b0
gyuri127 <117248176+gyuri127@users.noreply.github.com>
2024-12-20 14:40:49 +0900

    :sparkles: Feat: 동행 취소
    -동행 취소 구현 (일반 동행자)
    
    Related to: #37

--- src/main/java/com/example/festimo/domain/meet/service/CompanionService.java
+++ src/main/java/com/example/festimo/domain/meet/service/CompanionService.java
@@ -1,7 +1,7 @@
 package com.example.festimo.domain.meet.service;
 
-import com.example.festimo.domain.meet.entity.CompanionMemberId;
-import com.example.festimo.domain.meet.repository.CompanionMemberRepository;
+import com.example.festimo.domain.meet.entity.CompanionId;
+import com.example.festimo.domain.meet.repository.CompanionRepository;
 import com.example.festimo.exception.CustomException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -11,30 +11,30 @@ import static com.example.festimo.exception.ErrorCode.COMPANION_NOT_FOUND;
 @Service
 public class CompanionService {
 
-    private final CompanionMemberRepository companionMemberRepository;
+    private final CompanionRepository companionRepository;
 
-    public CompanionService(CompanionMemberRepository companionMemberRepository) {
-        this.companionMemberRepository = companionMemberRepository;
+    public CompanionService(CompanionRepository companionRepository) {
+        this.companionRepository = companionRepository;
     }
 
     /**
      * 동행 취소
      *
-     * @param companionId  취소하고 싶은 동행ID
+     * @param companyId  취소하고 싶은 동행ID
      * @param userId     취소하고 싶은 듀저 ID
      */
     @Transactional
-    public void deleteCompaion(Long companionId, Long userId) {
+    public void deleteCompaion(Long companyId, Long userId) {
 
-        CompanionMemberId companionMemberId = new CompanionMemberId(companionId, userId);
+        CompanionId companionId = new CompanionId(companyId, userId);
 
         //동행에 참가 여부 확인
-        if(!companionMemberRepository.existsById(companionMemberId)){
+        if(!companionRepository.existsById(companionId)){
             throw new CustomException(COMPANION_NOT_FOUND);
         }
 
         //동행원에서 삭제
-        companionMemberRepository.deleteById(companionMemberId);
+        companionRepository.deleteById(companionId);
     }
 
 }
