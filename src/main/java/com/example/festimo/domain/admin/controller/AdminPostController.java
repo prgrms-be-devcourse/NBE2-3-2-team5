dd59c2638b34db5d1590b133612d24cafe04e384
gyuri127 <117248176+gyuri127@users.noreply.github.com>
2024-12-22 18:06:09 +0900

    :art: Style: 스웨거 설명 변경
    - 스웨거 설명 변경
    
    Related to: #42

--- src/main/java/com/example/festimo/domain/admin/controller/AdminPostController.java
+++ src/main/java/com/example/festimo/domain/admin/controller/AdminPostController.java
@@ -1,13 +1,14 @@
 package com.example.festimo.domain.admin.controller;
 
 import com.example.festimo.domain.admin.service.AdminPostService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("admin/posts")
-@Tag(name = "관리자 API", description = "관리자가 게시글을 관리하는 API")
+@RequestMapping("/api/admin/posts")
+@Tag(name = "관리자 API")
 public class AdminPostController {
 
     private final AdminPostService adminPostService;
@@ -21,6 +22,7 @@ public class AdminPostController {
      * @param postId 삭제할 게시글 ID
      */
     @DeleteMapping("/{postId}")
+    @Operation(summary = "관리자의 게시글 삭제", description = "특정 게시글 삭제")
     public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
 
         adminPostService.deletePostById(postId);
