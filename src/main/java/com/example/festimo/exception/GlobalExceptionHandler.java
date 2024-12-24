26430ededd14251aa895ee1f129e6ad3bfafb66a
leehayeon <leehayeon200@naver.com>
2024-12-24 15:38:34 +0900

    :sparkles: feat: 예외 처리 로직 추가 및 수정
    - UnauthorizedException에 대한 예외 처리 핸들러 추가.
    - AccessDeniedException에 대한 예외 처리 핸들러 수정: 응답 메시지에 한글 설명 추가.
    
    Related to: #44

--- src/main/java/com/example/festimo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/festimo/exception/GlobalExceptionHandler.java
@@ -4,10 +4,8 @@ package com.example.festimo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.nio.file.AccessDeniedException;
@@ -52,14 +50,5 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.OK).body(errorResponse);
     }
 
-    @ExceptionHandler(AccessDeniedException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<Map<String, Object>> handleUnauthorizedException(UnauthorizedException ex) {
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("timestamp", LocalDateTime.now());
-        errorResponse.put("status", HttpStatus.FORBIDDEN.value());
-        errorResponse.put("error", "회원만 사용할 수 있는 기능입니다. [로그인] 또는 [회원가입] 후 다시 시도해주세요.");
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
-    }
-}
 
+}
