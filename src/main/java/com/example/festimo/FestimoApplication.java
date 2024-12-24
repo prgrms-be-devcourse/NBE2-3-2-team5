8f2bfee5ae31b7c76c4c54aaeea1802b56c6777c
leehayeon <leehayeon200@naver.com>
2024-12-24 15:37:43 +0900

    :sparkles: feat: 캐시 기능 활성화를 위한 @EnableCaching 어노테이션 추가
    - 애플리케이션에서 캐시 기능을 활성화하기 위해 @EnableCaching 어노테이션을 설정 클래스에 추가.
    
    Related to: #44

--- src/main/java/com/example/festimo/FestimoApplication.java
+++ src/main/java/com/example/festimo/FestimoApplication.java
@@ -3,11 +3,13 @@ package com.example.festimo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableScheduling
 @EnableJpaAuditing
+@EnableCaching
 @SpringBootApplication
 public class FestimoApplication {
     public static void main(String[] args) {
