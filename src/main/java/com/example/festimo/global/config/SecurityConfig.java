506350316c06a0be95e83eb25ceea1c1dff03feb
leehayeon <leehayeon200@naver.com>
2024-12-24 16:30:21 +0900

    :sparkles: feat: 게시물 관련 API 인증/인가 로직 수정
     - 게시물 전체 조회(비회원 허용) 및 상세 조회(인증 필요) 설정
     - 게시물 등록, 수정, 삭제, 댓글 등록/수정/삭제에 대한 인증 필요
    
    Related to: #44

--- src/main/java/com/example/festimo/global/config/SecurityConfig.java
+++ src/main/java/com/example/festimo/global/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.festimo.global.config;
 
-
 import com.example.festimo.global.utils.jwt.JwtAuthenticationFilter;
 import com.example.festimo.global.utils.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -20,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final AccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -33,23 +33,24 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/api/register", "/api/login").permitAll()
                         .requestMatchers(
+                                "/static/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
                         ).permitAll()
                         .requestMatchers("/error").permitAll()// 누구나 가능 , "/oauth2/**"
-                        .requestMatchers(HttpMethod.GET, "/api/companions").permitAll() // 게시글 전체 조회는 비회원 허용
-                        .requestMatchers(HttpMethod.GET, "/api/companions/{postId}").authenticated() // 게시글 상세 조회 인증 필요
-                        .requestMatchers(HttpMethod.POST, "/api/companions").authenticated() // 게시글 등록 인증 필요
-                        .requestMatchers(HttpMethod.PUT, "/api/companions/{postId}").authenticated() // 게시글 수정 인증 필요
-                        .requestMatchers(HttpMethod.DELETE, "/api/companions/{postId}").authenticated() // 게시글 삭제 인증 필요
-                        .requestMatchers(HttpMethod.POST, "/api/companions/{postId}/comments").authenticated() // 댓글 등록 인증 필요
-                        .requestMatchers(HttpMethod.PUT, "/api/companions/{postId}/comments/{sequence}").authenticated() // 댓글 수정 인증 필요
-                        .requestMatchers(HttpMethod.DELETE, "/api/companions/{postId}/comments/{sequence}").authenticated() // 댓글 삭제 인증 필요
+                        .requestMatchers(HttpMethod.GET, "/api/companions").permitAll()
+                        .requestMatchers(
+                                "/api/companions/{postId}/comments/**",  // 댓글 관련 모든 요청
+                                "/api/companions/{postId}/**"            // 게시글 상세 관련 모든 요청
+                        ).authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/companions").authenticated()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN") // 권한 기반 접근 제어 관리자만 사용 가능
-                        .anyRequest().authenticated()    // 나머지는 로그인한 사용자만
+                        .anyRequest().permitAll()    // 나머지는 로그인한 사용자만
+                )
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler(customAccessDeniedHandler)
                 );
 //                .oauth2Login()
 //                .defaultSuccessUrl("/api/oauth2/success");
